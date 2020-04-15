55fa5dd484f3ebc6e2695a90825904224b8a07e7
Raimundin <ricosigla@gmail.com>
2020-04-15 18:45:33 +0300

    Added Task model

---
+++ lib/models/task.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Task{
+	final String title;
+	final String subtitle;
+	final Color color;
+	//TODO add isCompleted (if it needed)
+
+	Task({this.title, this.subtitle, this.color});
+}
