065fa81d61ead50818af383cd775aaeb87e07f96
Raimundin <ricosigla@gmail.com>
2020-04-15 19:32:17 +0300

    Added Sign in Anonymously

---
+++ lib/services/auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+	final FirebaseAuth _auth = FirebaseAuth.instance;
+
+	//sign in anon
+	Future signInAnon() async{
+		try{
+		  AuthResult result = await _auth.signInAnonymously();
+		  FirebaseUser user = result.user;
+		  return user;
+		} catch (e){
+			print(e.toString());
+			return null;
+		}
+	}
+	//sign in email and password
+
+	//register email and password
+
+	//sign out
+}
